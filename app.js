d10b7b7c292ee741a8c837f24f6a604965cb71b4
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-27 22:28:25 +0600

    About and Contact Page Add Associate with Header and Footer.

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ app.get('/',function(req,res){
 })
 
 
+app.get('/about',function(req,res){
+  res.render('about', {startingAbout: aboutContent})
+})
+
+
+app.get('/contact',function(req,res){
+  res.render('contact', {startingContact: contactContent})
+})
+
+
+
 
 
 
